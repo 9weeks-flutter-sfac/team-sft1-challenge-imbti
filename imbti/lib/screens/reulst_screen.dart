0082676cbcc09de02b32daa63aa6ac2f84555f61
hedooss <gpwn0408@naver.com>
2023-10-04 17:31:25 +0900

    Co-authored-by: Jihoon Park <wlgnsdl815@users.noreply.github.com>

--- imbti/lib/screens/reulst_screen.dart
+++ imbti/lib/screens/reulst_screen.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:imbti/const/description_map.dart';
+import 'package:imbti/const/mbti_data.dart';
+import 'package:imbti/models/mbti_model.dart';
 import 'package:imbti/models/result_model.dart';
 import 'package:imbti/screens/test_screen.dart';
+import 'package:imbti/styles/app_colors.dart';
+import 'package:imbti/styles/app_text_style.dart';
 import 'package:imbti/widgets/main_button.dart';
 
 class ResultScreen extends StatelessWidget {
@@ -9,6 +14,8 @@ class ResultScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<MBTIModel> mbtiList =
+        mbtiData.map((data) => MBTIModel.fromMap(data)).toList();
     String src = '${resultModel.mbti}'.toLowerCase();
     return Scaffold(
       backgroundColor: Color(0xffF8F5F5),
@@ -97,7 +104,9 @@ class ResultScreen extends StatelessWidget {
                     Container(
                       alignment: Alignment.bottomRight,
                       child: TextButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          showDraggableBottomSheet(context, mbtiList);
+                        },
                         child: Text(
                           '자세히 보기 >',
                           style: TextStyle(color: Colors.grey[600]),
@@ -120,6 +129,69 @@ class ResultScreen extends StatelessWidget {
                   },
                   buttonText: '다시 검사하기'))
         ],
+      ),
+    );
+  }
+
+  void showDraggableBottomSheet(
+      BuildContext context, List<MBTIModel> mbtiList) {
+    showModalBottomSheet(
+      backgroundColor: Colors.transparent,
+      isScrollControlled: true,
+      context: context,
+      builder: (_) => DraggableScrollableSheet(
+        initialChildSize: 0.6,
+        maxChildSize: 0.9,
+        snap: true,
+        snapSizes: [0.6, 0.9],
+        builder: (BuildContext context, ScrollController scrollController) {
+          return Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Colors.white,
+            ),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 5, 0, 30),
+                  child: Container(
+                    width: 120,
+                    height: 5,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      color: AppColors.b1,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: ListView(
+                    shrinkWrap: true,
+                    controller: scrollController,
+                    children: [
+                      Column(
+                        children: [
+                          Text(
+                            resultModel.mbti,
+                            style: AppTextStyles.header24(),
+                          ),
+                          SizedBox(height: 20),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 10.0),
+                            child: Text(
+                              description[resultModel.mbti.toLowerCase()],
+                              style: AppTextStyles.regular,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
